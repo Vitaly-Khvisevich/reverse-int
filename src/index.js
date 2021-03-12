753b16acee04cbd5c78d1d7ee1d5d91b1f110541
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-03-12 10:04:12 +0300

    init: add reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+let x;
+let result=0
+if (Math.sign(n)==-1){
+    n=Math.abs(n);
+  }
+while(n){
+x=n%10;
+result = (result * 10) + x;
+n = n/10|0;
+}
+return result;
 }
